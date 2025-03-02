8d73248e6b658fe2e638cd27a4ac0674fe96c5a0
“M0ther0fGingers” <jen@robisons.us>
2025-03-02 10:19:42 -0600

    added bar chart and started flask

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,28 +1,38 @@
 # Import the dependencies.
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
-
+from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 # Declare a Base using `automap_base()`
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
-
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create a session
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
+app = Flask(__name__)
 
 
 
@@ -30,3 +40,20 @@
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    return("""Welcome to the Hawaii weather page!""")
+
+@app.route("/stations")
+def stations():
+    #station_list = session.query(Measurement.station,func.count(Measurement.station)).\
+    #group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+    # session.close()
+    return "help"
+#    station_list = list(np.ravel(station_list))
+    
+#    return jsonify(station_list)
+
+
+if __name__ == "__main__": 
+    app.run()
